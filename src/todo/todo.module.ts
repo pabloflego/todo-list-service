7bcec00fc32682645016eab9357435accd273632
Pablo Flego <pablinff@gmail.com>
2025-11-24 22:11:54 +0100

    feat: inject repository in todo module

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './todo.entity';
+import { TodoRepository } from './todo.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todo])],
-  providers: [],
+  providers: [TodoRepository],
   controllers: [],
 })
 export class TodoModule {}
