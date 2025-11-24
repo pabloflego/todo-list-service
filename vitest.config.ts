062b56f12537bb1362d71dbbe4e9fccbf6d49e63
Pablo Flego <pablinff@gmail.com>
2025-11-24 13:54:58 +0100

    chore: add vitest as test framework

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: 'node',
+    globals: true,
+    include: ['src/**/*.spec.ts'],
+    exclude: ['dist/**'],
+    setupFiles: ['reflect-metadata'],
+    coverage: { provider: 'v8', reportsDirectory: './coverage' },
+  },
+});
