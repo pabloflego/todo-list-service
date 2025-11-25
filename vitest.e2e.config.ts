fcbd3b5f98d5b74b970ff912c61347bf4275092f
Pablo Flego <pablinff@gmail.com>
2025-11-25 17:01:43 +0100

    chore: added e2e config + updated scripts

---
+++ vitest.e2e.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import swc from 'unplugin-swc';
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), swc.vite()],
+  test: {
+    environment: 'node',
+    globals: true,
+    include: ['e2e/**/*.e2e.spec.ts'],
+    exclude: ['dist/**'],
+    setupFiles: ['reflect-metadata'],
+    coverage: { provider: 'v8', reportsDirectory: './coverage/e2e' },
+  },
+});
